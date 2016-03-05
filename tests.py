48490654454f0b2e858e0db88de73259116554cd
Elsa Birch <elsabirch@users.noreply.github.com>
2016-03-05 13:10:47 -0800

    Existing tests working again after moving functions between modules.

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 import server
+import utilities
 import doctest
 
 
@@ -11,34 +12,34 @@ def load_tests(loader, tests, ignore):
     return tests
 
 
-class ServerHelperFunctionsTestCase(unittest.TestCase):
+class UtilitiesParserFunctionsTestCase(unittest.TestCase):
 
     def test_to_clean_string_from_input(self):
 
         # Case for string already clean
         test_in = 'foo13ar'
         expect_out = 'foo13ar'
-        self.assertEqual(server.to_clean_string_from_input(test_in, 100),
+        self.assertEqual(utilities.to_clean_string_from_input(test_in, 100),
                          expect_out)
 
         # Case for string not clean
         test_in = 'fo*o13!ar; droptables'
         expect_out = 'foo13ardroptables'
-        self.assertEqual(server.to_clean_string_from_input(test_in, 100),
+        self.assertEqual(utilities.to_clean_string_from_input(test_in, 100),
                          expect_out)
 
         # Case for string only unclean
         test_in = '&*#%;+'
-        self.assertIsNone(server.to_clean_string_from_input(test_in, 100))
+        self.assertIsNone(utilities.to_clean_string_from_input(test_in, 100))
 
         # Case for string empty
         test_in = ''
-        self.assertIsNone(server.to_clean_string_from_input(test_in, 100))
+        self.assertIsNone(utilities.to_clean_string_from_input(test_in, 100))
 
         # Case for string unclean and too long
         test_in = 'fo*o13!ar; droptables'
         expect_out = 'foo'
-        self.assertEqual(server.to_clean_string_from_input(test_in, 3),
+        self.assertEqual(utilities.to_clean_string_from_input(test_in, 3),
                          expect_out)
 
     def test_to_float_from_input(self):
@@ -46,51 +47,51 @@ class ServerHelperFunctionsTestCase(unittest.TestCase):
         # Case for spaces
         test_in = '  13 '
         expect_out = 13.0
-        self.assertEqual(server.to_float_from_input(test_in),
+        self.assertEqual(utilities.to_float_from_input(test_in),
                          expect_out)
 
         # Case for other characters arround
         test_in = 'foo13*ar'
         expect_out = 13.0
-        self.assertEqual(server.to_float_from_input(test_in),
+        self.assertEqual(utilities.to_float_from_input(test_in),
                          expect_out)
 
         # Case for decimals
         test_in = '13.14'
         expect_out = 13.14
-        self.assertEqual(server.to_float_from_input(test_in),
+        self.assertEqual(utilities.to_float_from_input(test_in),
                          expect_out)
 
         # Case for zero pad
         test_in = '00013'
         expect_out = 13.0
-        self.assertEqual(server.to_float_from_input(test_in),
+        self.assertEqual(utilities.to_float_from_input(test_in),
                          expect_out)
 
         # Case for empty
         test_in = ''
-        self.assertIsNone(server.to_float_from_input(test_in))
+        self.assertIsNone(utilities.to_float_from_input(test_in))
 
         # Case for all unclean
         test_in = 'juihiuhr&&*@'
-        self.assertIsNone(server.to_float_from_input(test_in))
+        self.assertIsNone(utilities.to_float_from_input(test_in))
 
         # Case for negative
         test_in = '-13.0 '
         expect_out = -13.0
-        self.assertEqual(server.to_float_from_input(test_in),
+        self.assertEqual(utilities.to_float_from_input(test_in),
                          expect_out)
 
         # Case for fractional
         test_in = '0.13'
         expect_out = 0.13
-        self.assertEqual(server.to_float_from_input(test_in),
+        self.assertEqual(utilities.to_float_from_input(test_in),
                          expect_out)
 
         # Case for zero
         test_in = '-0.0 '
         expect_out = 0.0
-        self.assertAlmostEqual(server.to_float_from_input(test_in),
+        self.assertAlmostEqual(utilities.to_float_from_input(test_in),
                                expect_out)
 
 
@@ -100,12 +101,25 @@ class ServerRoutesTestCase(unittest.TestCase):
         self.client = server.app.test_client()
         server.app.config['TESTING'] = True
 
-    def test_navigation(self):
+    def test_navigation_logged_out(self):
 
         result = self.client.get('/navigation')
-        self.assertIn('(pick one to be arranged on a wall)', result.data)
+        self.assertIn('Galleries', result.data)
+        self.assertIn('Walls', result.data)
+        self.assertNotIn('Curate', result.data)
+        self.assertNotIn('Upload', result.data)
 
-    # def navigation while logged in
+    # def test_navigation_logged_in(self):
+
+    #     server.app.session['user_id'] = 13
+    #     server.app.session['username'] = 'foo'
+
+    #     result = self.client.get('/navigation')
+    #     self.assertIn('Galleries', result.data)
+    #     self.assertIn('Walls', result.data)
+    #     self.assertNotIn('Curate', result.data)
+    #     self.assertNotIn('Upload', result.data)
+
 
     # def navigations while not logged in
 
